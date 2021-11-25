6afc87a207dedf50687b5d84b98f52c02e1afc9d
Mark Vousden <m.vousden@soton.ac.uk>
2021-11-25 14:40:15 +0000

    Add terse example usage

---
+++ example.py
@@ -0,0 +1,4 @@
+import placement_postprocessing as pp
+data = pp.Data("/home/mark/repos/orchestrator/Output/Placement")
+pp.mailbox_loading_histogram(data).savefig("mailbox_loading.pdf")
+pp.core_loading_histogram(data).savefig("core_loading.pdf")
--- ohdear.py
+++
@@ -1,6 +0,0 @@
-import placement_postprocessing as pp
-data = pp.Data("/home/mark/repos/orchestrator/Output/Placement")
-print(data.frames["hardware core loading"])
-print("and")
-print(data.frames["hardware mailbox loading"])
-# It works!
