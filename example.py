8637ef59e9bcf8bdacfc0db7fda57ae02c6b816f
Mark Vousden <m.vousden@soton.ac.uk>
2021-11-26 10:03:21 +0000

    Update example to use new stuff.

--- example.py
+++ example.py
@@ -1,5 +1,8 @@
 import placement_postprocessing as pp
 data = pp.Data("/home/mark/repos/orchestrator/Output/Placement")
 pp.mailbox_loading_histogram(data).savefig("mailbox_loading.pdf")
-pp.core_loading_histogram(data).savefig("core_loading.pdf")
-pp.edge_loading_histogram(data).savefig("edge_loading.pdf")
+pp.core_loading_histogram(data,
+                          plotArgs={"bins": 4}).savefig("core_loading.pdf")
+pp.mailbox_edge_loading_histogram(data,
+                                  plotArgs={"bins": 8}).savefig("edge_loading.pdf")
+pp.application_edge_cost_histogram(data).savefig("app_edge_costs.pdf")
