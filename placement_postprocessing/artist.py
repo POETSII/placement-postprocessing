943c638f7ad6e69c55d339adeac5613280f1787f
Mark Vousden <m.vousden@soton.ac.uk>
2021-11-26 10:03:12 +0000

    Plot application edge costs, and use the number of requested bins to parameterise the horizontal axis.

--- placement_postprocessing/artist.py
+++ placement_postprocessing/artist.py
@@ -5,6 +5,7 @@ from .data import Data
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def _common_formatting(figure: mpl.figure.Figure) -> mpl.figure.Figure:
@@ -12,7 +13,10 @@ def _common_formatting(figure: mpl.figure.Figure) -> mpl.figure.Figure:
     Take a figure and apply sensible formatting to to it.
     """
     axes = figure.gca()
-    axes.xaxis.get_major_locator().set_params(integer=True)
+    try:
+        axes.xaxis.get_major_locator().set_params(integer=True)
+    except TypeError:
+        pass
     axes.yaxis.get_major_locator().set_params(integer=True)
     axes.spines["right"].set_visible(False)
     axes.spines["top"].set_visible(False)
@@ -22,30 +26,15 @@ def _common_formatting(figure: mpl.figure.Figure) -> mpl.figure.Figure:
     return figure
 
 
-# Alias
-def core_loading_histogram(data: Data, figArgs: dict={},
-                           plotArgs: dict={}) \
-    -> mpl.figure.Figure:
-    """See documentation for hardware_loading_histogram."""
-    return hardware_loading_histogram(data, "core", figArgs, plotArgs)
-
-
-def edge_loading_histogram(data: Data, figArgs: dict={},
-                           plotArgs: dict={}) \
-    -> mpl.figure.Figure:
-    """See documentation for hardware_loading_histogram."""
-    return hardware_loading_histogram(data, "edge", figArgs, plotArgs)
-
-
-def hardware_loading_histogram(data: Data, what: str="mailbox",
-                           figArgs: dict={}, plotArgs: dict={}) \
-    -> mpl.figure.Figure:
+def _histogram(data: Data, what: str="mailbox", figArgs: dict={},
+               plotArgs: dict={}) -> mpl.figure.Figure:
     """
-    Draws a loading histogram - either for cores (node), for mailboxes (node),
-    or for edges connecting mailboxes. Arguments:
+    Draws a histogram - either for cores (node), for mailboxes (node), for
+    edges connecting mailboxes (hwedge), or for the cost associated with each
+    application edge (appedge). Arguments:
 
     - data: A placement_processing Data object with loaded data.
-    - what: String, either "mailbox", "core", or "edge"
+    - what: String, either "mailbox", "core", "hwedge", or "appedge".
     - figArgs: Keyword arguments to be passed to the figure constructor
           (subplots).
     - plotArgs: Plotting arguments to be passed to the histogram plot (hist).
@@ -58,11 +47,13 @@ def hardware_loading_histogram(data: Data, what: str="mailbox",
         loadings = data.frames[keyNodeLoadingCore]["load"]
     elif what == "mailbox":
         loadings = data.frames[keyNodeLoadingMbox]["load"]
-    elif what == "edge":
+    elif what == "hwedge":
         loadings = data.frames[keyHwEdgeLoading]["load"]
+    elif what == "appedge":
+        loadings = data.frames[keyAppEdgeCosts]["cost"]
     else:  # Sanity
         raise RuntimeError("Argument 'what' must be either 'mailbox', 'core', "
-                           "or 'edge'.")
+                           "'hwedge', or 'appedge'.")
 
     # Process figure arguments (adding our defaults). We modify inplace because
     # we're monsters.
@@ -72,9 +63,16 @@ def hardware_loading_histogram(data: Data, what: str="mailbox",
             figArgs[item[0]] = item[1]
 
     # Process plot arguments (adding our defaults), as before.
-    defaultPlotArgs = {"bins": 6,
-                       "color": "b" if what == "edge" else "r",
-                       "edgecolor": "#000055" if what == "edge" else "#550000"}
+    defaultPlotArgs = {"bins": 6}
+    if what == "hwedge":
+        defaultPlotArgs["color"] = "b"
+        defaultPlotArgs["edgecolor"] = "#000055"
+    elif what == "appedge":
+        defaultPlotArgs["color"] = "g"
+        defaultPlotArgs["edgecolor"] = "#005500"
+    else:
+        defaultPlotArgs["color"] = "r"
+        defaultPlotArgs["edgecolor"] = "#550000"
     for item in defaultPlotArgs.items():
         if item[0] not in plotArgs.keys():
             plotArgs[item[0]] = item[1]
@@ -88,6 +86,14 @@ def hardware_loading_histogram(data: Data, what: str="mailbox",
     axes.set_xlim(min(loadings), max(loadings +
                   (0 if min(loadings) != max(loadings) else 1e-2)))
 
+    # Line up ticks with bins, formatting them as integers if possible.
+    desiredTicks = np.linspace(min(loadings), max(loadings),
+                               plotArgs["bins"] + 1)
+    if (desiredTicks == desiredTicks.astype(int)).all():
+        axes.set_xticks(desiredTicks.astype(int))
+    else:
+        axes.set_xticks(desiredTicks)
+
     # The formatting continues...
     axes.set_ylim(0, len(loadings))
     axes.set_ylabel("Occurences (total={})".format(len(loadings)))
@@ -97,16 +103,43 @@ def hardware_loading_histogram(data: Data, what: str="mailbox",
     elif what == "mailbox":
         axes.set_xlabel("Number of application nodes placed on mailboxes")
         axes.set_title("Mailbox Loading")
-    elif what == "edge":
+    elif what == "hwedge":
         axes.set_xlabel("Number of application edges overlaying\n "
                         "mailbox edges (in hardware)")
         axes.set_title("Mailbox Edge Loading")
+    elif what == "appedge":
+        axes.set_xlabel("Edge cost")
+        axes.set_title("'Hardware cost' of application edges")
     return _common_formatting(figure)
+
+
+# Alias
+def application_edge_cost_histogram(data: Data, figArgs: dict={},
+                                    plotArgs: dict={}) \
+    -> mpl.figure.Figure:
+    """See documentation for _histogram."""
+    return _histogram(data, "appedge", figArgs, plotArgs)
+
+
+# Alias
+def core_loading_histogram(data: Data, figArgs: dict={},
+                           plotArgs: dict={}) \
+    -> mpl.figure.Figure:
+    """See documentation for _histogram."""
+    return _histogram(data, "core", figArgs, plotArgs)
+
+
+# Alias
+def mailbox_edge_loading_histogram(data: Data, figArgs: dict={},
+                                   plotArgs: dict={}) \
+    -> mpl.figure.Figure:
+    """See documentation for _histogram."""
+    return _histogram(data, "hwedge", figArgs, plotArgs)
 
 
 # Alias
 def mailbox_loading_histogram(data: Data, figArgs: dict={},
                               plotArgs: dict={}) \
     -> mpl.figure.Figure:
-    """See documentation for hardware_loading_histogram."""
-    return hardware_loading_histogram(data, "mailbox", figArgs, plotArgs)
+    """See documentation for _histogram."""
+    return _histogram(data, "mailbox", figArgs, plotArgs)
