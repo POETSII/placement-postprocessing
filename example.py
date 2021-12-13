b4ac18ff2fa47f1178a6cb2f5690ffdec7858b9b
Mark Vousden <m.vousden@soton.ac.uk>
2021-12-13 18:06:46 +0000

    Clean up after ourselves.

--- example.py
+++ example.py
@@ -8,4 +8,4 @@ pp.core_loading_histogram(data,
 pp.mailbox_edge_loading_histogram(data,
                                   plotArgs={"bins": 8}).savefig("edge_loading.pdf")
 pp.application_edge_cost_histogram(data).savefig("app_edge_costs.pdf")
-pp.draw_map(data, outPath="map.pdf", cleanup=False)
+pp.draw_map(data, outPath="map.pdf")
